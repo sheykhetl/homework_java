630593aa6a29dae8be10f8d58c88a15a0fbfefe3
sheykhetl <31258159+sheykhetl@users.noreply.github.com>
2021-12-07 14:23:03 +0300

    Delete dz24 directory

--- dz24/dz24_1/src/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-
-    public static void main(String[] args) {
-        System.out.println(new person("Ivanov", "Ivan", "Jovanovich"));
-        System.out.println(new person("Ivanov","Ivan", null));
-        System.out.println(new person("Ivanov", null, null));
-    }
-
-}
